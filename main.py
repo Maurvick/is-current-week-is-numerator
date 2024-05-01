5bd92fad4e40a686e3861380aaef1d49592b778e
Maurvick <82706197+Maurvick@users.noreply.github.com>
2024-05-01 11:54:57 +0300

    disable first day check

--- main.py
+++ main.py
@@ -4,22 +4,26 @@ def main():
     month = date.today().strftime("%m")
     year = date.today().strftime("%y")
     day = date.today().strftime("%d")
-    dayname = getDay(year, month)
+    dayname = GetFirstDayOfTheWeek(year, month)
     if (checkWeek(dayname, day)):
-        print("numerator")
-    else: 
         print("denominator")
+    else: 
+        print("numerator")
 
-def getDay(year, month):
+def GetFirstDayOfTheWeek(year, month):
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     first_day_of_the_month = date(int(year), int(month), 1).weekday()
     return days[first_day_of_the_month]
 
+def isMonthStartingFromMonday(dayname):
+    if (dayname != "Monday"): 
+        return False
+    else:
+        return True
+
 def checkWeek(dayname, day):
     is_numerator = True
     days = int(day)
-    if (dayname != "Monday"): 
-        is_numerator = False
     weeks = float(days / 7) 
     if (weeks % 2 != 0):
         return not is_numerator
